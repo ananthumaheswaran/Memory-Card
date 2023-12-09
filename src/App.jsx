7de21217419f9cfd1ee0584a5bd6250edd515ab7
ANANTHU M S <ananthumaheswaran098@gmail.com>
2023-12-09 13:13:52 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Cards from "./components/Cards";
+
 function App() {
   return (
     <>
       <div>
         <h1>Application Under Construction</h1>
+        <Cards />
       </div>
     </>
   );
