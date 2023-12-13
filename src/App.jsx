e9f5d1f7ef2d4614796d07e9f64b77bf4d362986
ANANTHU M S <ananthumaheswaran098@gmail.com>
2023-12-13 06:02:11 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Cards from "./components/Cards";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <>
       <div className="game-home">
         <h1>Application Under Construction</h1>
-        <Cards />
+        <Pokemon />
       </div>
     </>
   );
