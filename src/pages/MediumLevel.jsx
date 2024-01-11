667372ee1dbd91f58afc8d11d64ce5c0b8609860
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-11 23:23:19 +0530

    Medium level created

---
+++ src/pages/MediumLevel.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import Fetch from "../components/Fetch";
+
+const MediumLevel = () => {
+  const [fetchStart, setFetchStart] = useState(false);
+  const handleButtonClick = () => {
+    setFetchStart(true);
+  };
+  const handleRestButtonClick = () => {
+    setFetchStart(false);
+  };
+  return (
+    <div className="medium-level">
+      <Button
+        typeOf="button"
+        buttonText="Medium"
+        classForStyling="btn"
+        onClick={handleButtonClick}
+      />
+      {fetchStart && (
+        <div>
+          <div>
+            <Fetch numberOfPokemon={10} />
+          </div>
+          <div>
+            <Button
+              typeOf="button"
+              buttonText="Exit"
+              classForStyling="btn"
+              onClick={handleRestButtonClick}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MediumLevel;
