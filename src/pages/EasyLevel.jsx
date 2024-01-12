7c34eb60b8ee25c735fe58a39706f75e05bb03e6
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-12 23:56:00 +0530

    updated

--- src/pages/EasyLevel.jsx
+++ src/pages/EasyLevel.jsx
@@ -7,28 +7,46 @@ const EasyLevel = () => {
   const handleButtonClick = () => {
     setFetchStart(true);
   };
-  const handleRestButtonClick = () => {
+  const handleResetButtonClick = () => {
     setFetchStart(false);
   };
   return (
     <div className="easy-level">
-      <Button
-        typeOf="button"
-        buttonText="Easy"
-        classForStyling="btn"
-        onClick={handleButtonClick}
-      />
+      {!fetchStart && (
+        <Button
+          typeOf="button"
+          buttonText="Start"
+          classForStyling="btn"
+          onClick={handleButtonClick}
+        />
+      )}
+
       {fetchStart && (
-        <div>
-          <div>
+        <div className="fetched-section">
+          <div className="fetched-cards">
             <Fetch numberOfPokemon={5} />
           </div>
-          <div>
+          <div className="exit-btn-container">
             <Button
               typeOf="button"
-              buttonText="Exit"
-              classForStyling="btn"
-              onClick={handleRestButtonClick}
+              buttonText={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+                  />
+                </svg>
+              }
+              classForStyling="btn-exit"
+              onClick={handleResetButtonClick}
             />
           </div>
         </div>
