9607afb0e97b8a2636961d1d4261847001ab4a4d
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-12 23:54:51 +0530

    all codes are commented out

--- src/pages/HardLevel.jsx
+++ src/pages/HardLevel.jsx
@@ -1,40 +1,43 @@
-import { useState } from "react";
-import Button from "../components/Button";
-import Fetch from "../components/Fetch";
+// import { useState } from "react";
+// import Button from "../components/Button";
+// import Fetch from "../components/Fetch";
 
-const HardLevel = () => {
-  const [fetchStart, setFetchStart] = useState(false);
-  const handleButtonClick = () => {
-    setFetchStart(true);
-  };
-  const handleRestButtonClick = () => {
-    setFetchStart(false);
-  };
-  return (
-    <div className="hard-level">
-      <Button
-        typeOf="button"
-        buttonText="Hard"
-        classForStyling="btn"
-        onClick={handleButtonClick}
-      />
-      {fetchStart && (
-        <div>
-          <div>
-            <Fetch numberOfPokemon={15} />
-          </div>
-          <div>
-            <Button
-              typeOf="button"
-              buttonText="Exit"
-              classForStyling="btn"
-              onClick={handleRestButtonClick}
-            />
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+// const HardLevel = () => {
+//   const [fetchStart, setFetchStart] = useState(false);
+//   const handleButtonClick = () => {
+//     setFetchStart(true);
+//   };
+//   const handleResetButtonClick = () => {
+//     setFetchStart(false);
+//   };
+//   return (
+//     <div className="hard-level">
+//       {!fetchStart && (
+//         <Button
+//           typeOf="button"
+//           buttonText="Hard"
+//           classForStyling="btn"
+//           onClick={handleButtonClick}
+//         />
+//       )}
 
-export default HardLevel;
+//       {fetchStart && (
+//         <div>
+//           <div>
+//             <Fetch numberOfPokemon={15} />
+//           </div>
+//           <div>
+//             <Button
+//               typeOf="button"
+//               buttonText="Exit"
+//               classForStyling="btn"
+//               onClick={handleResetButtonClick}
+//             />
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default HardLevel;
