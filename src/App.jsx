a7580c4d75bc0b018d9243e680ccf07db5cf42eb
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-15 23:40:41 +0530

    core component imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import Core from "./components/Core";
 function App() {
   return (
     <div>
       <Header />
+      <Core />
       <Cards />
     </div>
   );
