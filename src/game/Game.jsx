5fb5d5ef39d8d6b2ae280ef1964347a3490ab1c8
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-18 19:35:03 +0530

    fetchStart useState was created and imported Menu and Header components

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -1,5 +1,16 @@
+import Menu from "../components/Menu";
+import Header from "../components/Header";
+import { useState } from "react";
 const Game = () => {
-  return <div>Game</div>;
+  const [fetchStart, setFetchStart] = useState(false);
+  return (
+    <div>
+      <div>
+        <Header setFetchStart={setFetchStart} />
+        <Menu fetchStart={fetchStart} setFetchStart={setFetchStart} />
+      </div>
+    </div>
+  );
 };
 
 export default Game;
