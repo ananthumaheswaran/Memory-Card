8aa0ae453d460a872b569c8b931f221b13860474
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-18 19:33:16 +0530

    fetchStart state passed as a prop

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Button from "./Button";
 import Core from "./Core";
-const Menu = () => {
+const Menu = ({ fetchStart, setFetchStart }) => {
   const [cardLimit, setCardLimit] = useState(null);
+
   const handleLevelDifficulty = (limit) => {
     setCardLimit(limit);
   };
@@ -11,49 +12,52 @@ const Menu = () => {
   };
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div className="bg-white bg-white/40 m-20 p-10 rounded-xl flex items-center justify-center flex-col">
-          <div className="flex items-center justify-center mt-5 border-2 border-black/30 rounded-xl">
-            <div className="w-40 h-20 flex items-center justify-center">
-              <Button
-                buttonText="Easy"
-                classForStyling="btn"
-                typeOf="button"
-                onClick={() => handleLevelDifficulty(4)}
-              />
+      {!fetchStart ? (
+        <div className="flex items-center justify-center">
+          <div className="bg-white bg-white/30 mx-20 my-10 p-10 rounded-xl flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center mt-5 rounded-xl">
+              <div className="w-40 h-20 flex items-center justify-center">
+                <Button
+                  buttonText="Easy"
+                  classForStyling="btn"
+                  typeOf="button"
+                  onClick={() => handleLevelDifficulty(4)}
+                />
+              </div>
+              <div className="w-40 h-20 flex items-center justify-center">
+                <Button
+                  buttonText="Medium"
+                  classForStyling="btn"
+                  typeOf="button"
+                  onClick={() => handleLevelDifficulty(8)}
+                />
+              </div>
+              <div className="w-40 h-20 flex items-center justify-center">
+                <Button
+                  buttonText="Hard"
+                  classForStyling="btn"
+                  typeOf="button"
+                  onClick={() => handleLevelDifficulty(16)}
+                />
+              </div>
             </div>
-            <div className="w-40 h-20 flex items-center justify-center">
-              <Button
-                buttonText="Medium"
-                classForStyling="btn"
-                typeOf="button"
-                onClick={() => handleLevelDifficulty(8)}
-              />
-            </div>
-            <div className="w-40 h-20 flex items-center justify-center">
-              <Button
-                buttonText="Hard"
-                classForStyling="btn"
-                typeOf="button"
-                onClick={() => handleLevelDifficulty(16)}
-              />
-            </div>
-          </div>
-          <div className="flex items-center justify-center ">
-            <div className="w-40 h-20 flex items-center justify-center">
-              <Button
-                buttonText="Start"
-                classForStyling="btn"
-                typeOf="button"
-                onClick={handleFetching}
-              />
+            <div className="flex items-center justify-center ">
+              <div className="w-40 h-20 flex items-center justify-center">
+                <Button
+                  buttonText="Start"
+                  classForStyling="btn"
+                  typeOf="button"
+                  onClick={handleFetching}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <Core cardLimit={cardLimit} />
-      </div>
+      ) : (
+        <div className="flex items-center justify-center mt-10">
+          <Core cardLimit={cardLimit} />
+        </div>
+      )}
     </>
   );
 };
