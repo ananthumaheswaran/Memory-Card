955fbaf86bdee79a53f6e46204d55325e3d1f51f
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-19 22:02:09 +0530

    image path updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({ setFetchStart }) => {
         Mem
         <img
           className="header-image"
-          src="public\images\pokeball.png"
+          src=".\images\pokeball.png"
           alt="poke-ball"
         />
         ry
