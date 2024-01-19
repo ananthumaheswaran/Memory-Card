31677a922ceaeaf74f90a8778234536a3269880c
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-19 21:34:08 +0530

    styles updated

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,24 +2,26 @@ import { useState } from "react";
 import Button from "./Button";
 import Core from "./Core";
 const Menu = ({ fetchStart, setFetchStart }) => {
-  const [cardLimit, setCardLimit] = useState(null);
+  const [cardLimit, setCardLimit] = useState(null); // New state for set card limit in game play
 
   const handleLevelDifficulty = (limit) => {
     setCardLimit(limit);
   };
-  const handleFetching = () => {
+  const handleFetch = () => {
+    // Arrow function created to toggle fetch data from api
     setFetchStart(true);
   };
   return (
     <>
       {!fetchStart ? (
-        <div className="flex items-center justify-center">
-          <div className="bg-white bg-white/30 mx-20 my-10 p-10 rounded-xl flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center mx-20 my-10 bg-black/30 rounded-xl ">
+          <div className="bg-white/70  p-10 rounded-xl flex items-center justify-center flex-col">
+            <h1 className="text-black text-4xl">Let's Start!</h1>
             <div className="flex items-center justify-center mt-5 rounded-xl">
               <div className="w-40 h-20 flex items-center justify-center">
                 <Button
                   buttonText="Easy"
-                  classForStyling="btn"
+                  classForStyling="btn-easy"
                   typeOf="button"
                   onClick={() => handleLevelDifficulty(4)}
                 />
@@ -27,7 +29,7 @@ const Menu = ({ fetchStart, setFetchStart }) => {
               <div className="w-40 h-20 flex items-center justify-center">
                 <Button
                   buttonText="Medium"
-                  classForStyling="btn"
+                  classForStyling="btn-medium"
                   typeOf="button"
                   onClick={() => handleLevelDifficulty(8)}
                 />
@@ -35,7 +37,7 @@ const Menu = ({ fetchStart, setFetchStart }) => {
               <div className="w-40 h-20 flex items-center justify-center">
                 <Button
                   buttonText="Hard"
-                  classForStyling="btn"
+                  classForStyling="btn-hard"
                   typeOf="button"
                   onClick={() => handleLevelDifficulty(16)}
                 />
@@ -47,7 +49,7 @@ const Menu = ({ fetchStart, setFetchStart }) => {
                   buttonText="Start"
                   classForStyling="btn"
                   typeOf="button"
-                  onClick={handleFetching}
+                  onClick={handleFetch}
                 />
               </div>
             </div>
@@ -55,7 +57,7 @@ const Menu = ({ fetchStart, setFetchStart }) => {
         </div>
       ) : (
         <div className="flex items-center justify-center mt-10">
-          <Core cardLimit={cardLimit} />
+          <Core cardLimit={cardLimit} setFetchStart={setFetchStart} />
         </div>
       )}
     </>
