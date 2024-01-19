5f2aebc7d70602e8152204549d05e0782445fc24
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-20 01:20:57 +0530

    updated

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -2,11 +2,11 @@ import Menu from "../components/Menu";
 import Header from "../components/Header";
 import { useState } from "react";
 const Game = () => {
-  const [fetchStart, setFetchStart] = useState(false);
+  const [fetchStart, setFetchStart] = useState(false); // State for display fetched data
   return (
     <div>
       <div className="flex items-center flex-col justify-center">
-        {/* Imported Header component and Menu Component  */}
+        {/* Imported Header component and Menu Component to display  */}
         <Header setFetchStart={setFetchStart} />
         <Menu fetchStart={fetchStart} setFetchStart={setFetchStart} />
       </div>
