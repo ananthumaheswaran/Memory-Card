1d6d5aa305b2ac56543b256ed8facfbe0ab18990
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-23 22:44:35 +0530

    removed repeating divs

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,34 +12,33 @@ const Menu = ({ fetchStart, setFetchStart }) => {
   return (
     <>
       {!fetchStart ? (
-        <div className="flex items-center justify-center mx-20 my-10 bg-black/20 rounded-xl ">
-          <div className="bg-white/70  p-10 rounded-xl flex items-center justify-center flex-col">
-            <h1 className="text-black text-4xl">Let's Start!</h1>
-            <div className="flex items-center justify-center mt-5 rounded-xl">
-              <div className="w-40 h-20 flex items-center justify-center">
-                <Button
-                  buttonText="Easy"
-                  classForStyling="btn-easy"
-                  typeOf="button"
-                  onClick={() => handleLevelDifficulty(4)}
-                />
-              </div>
-              <div className="w-40 h-20 flex items-center justify-center">
-                <Button
-                  buttonText="Medium"
-                  classForStyling="btn-medium"
-                  typeOf="button"
-                  onClick={() => handleLevelDifficulty(8)}
-                />
-              </div>
-              <div className="w-40 h-20 flex items-center justify-center">
-                <Button
-                  buttonText="Hard"
-                  classForStyling="btn-hard"
-                  typeOf="button"
-                  onClick={() => handleLevelDifficulty(16)}
-                />
-              </div>
+        <div className="menu-outer-container">
+          <div className="menu-inner-container">
+            <h1 className="menu-heading">Let's Start!</h1>
+            <div className="md:flex items-center md:justify-center mt-5 rounded-xl sm:items-start">
+              <Button
+                buttonText="Easy"
+                classForStyling="btn"
+                btnContainerStyle="btn-container"
+                typeOf="button"
+                onClick={() => handleLevelDifficulty(4)}
+              />
+
+              <Button
+                buttonText="Medium"
+                classForStyling="btn"
+                btnContainerStyle="btn-container"
+                typeOf="button"
+                onClick={() => handleLevelDifficulty(8)}
+              />
+
+              <Button
+                buttonText="Hard"
+                classForStyling="btn"
+                btnContainerStyle="btn-container"
+                typeOf="button"
+                onClick={() => handleLevelDifficulty(16)}
+              />
             </div>
           </div>
         </div>
